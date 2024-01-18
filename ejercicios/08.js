faea02d0d1f6c234209d9bb179cf87a6dd99f43b
francisco javier <frankmazta.gmail.com>
2024-01-18 10:47:04 -0400

    complete el ejercio  07.js y inicie en 08.js

---
+++ ejercicios/08.js
@@ -0,0 +1,22 @@
+let array =[{
+     id: 1,
+     name: 'nicolas',
+},{
+     id: 2,
+     name:'felipe',
+},{
+     id: 3,
+     name:'chanchito',
+}];
+
+let pares = [
+     [1, { id: 1, name: "nicolas" }],
+     [2, { id: 2, name: "felipe" }],
+     [3, { id: 3, name: "chanchito" }],
+];
+
+function toPairs(arr){
+
+}
+let resultado = toPairs(array);
+console.log(resultado);
