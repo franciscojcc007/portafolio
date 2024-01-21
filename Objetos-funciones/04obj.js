589b1e9f3e7fb08ed115ef1b0760c4fee1c893db
francisco javier <frankmazta.gmail.com>
2024-01-21 12:08:08 -0400

    se le añadio como ver la propiedades de un objectos

--- Objetos-funciones/04obj.js
+++ Objetos-funciones/04obj.js
@@ -1,14 +1,10 @@
 
 function Automovil(nombre){
      this.nombre = nombre;
-
 }
 
-console.log(Automovil.nombre);
-
-const A = Automovil
+const A = Automovil;
 let Aut = new A("Toyota");
-
 console.log(Aut);
 
 function of(Fn, arg){
@@ -17,3 +13,34 @@ function of(Fn, arg){
 
 let auto1 = of(Automovil, "mazda");
 console.log(auto1);
+
+
+
+
+const punto = {
+     x: 4,
+     y: 10,
+     dibujar(){
+          console.log("dibujando un ....");
+     }
+
+};
+if('dibujar' in punto){
+     punto.dibujar();
+}
+
+//ver la propiedades de un objecto
+//vieja forma
+for(let llave of Object.keys(punto)){
+     console.log(llave, punto[llave]);
+}
+
+for(let entry of Object.entries(punto)){
+     console.log(entry);
+}
+
+
+//nueva forma
+for(let llave in punto){
+     console.log(llave, punto[llave]);
+}
