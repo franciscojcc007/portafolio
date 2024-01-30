34cbd5b227273ae1be2bdd032b89c6c00871f5ea
francisco javier <frankmazta.gmail.com>
2024-01-30 17:11:15 -0400

    modificacion generar

--- Objetos-funciones/04obj.js
+++ Objetos-funciones/04obj.js
@@ -1,46 +1,53 @@
 
-function Automovil(nombre){
-     this.nombre = nombre;
-}
+// function Automovil(nombre){
+//      this.nombre = nombre;
+// }
 
-const A = Automovil;
-let Aut = new A("Toyota");
-console.log(Aut);
+// const A = Automovil;
+// let Aut = new A("Toyota");
+// console.log(Aut);
 
-function of(Fn, arg){
-     return new Fn(arg);
-};
+// function of(Fn, arg){
+//      return new Fn(arg);
+// };
 
-let auto1 = of(Automovil, "mazda");
-console.log(auto1);
-
-
+// let auto1 = of(Automovil, "mazda");
+// console.log(auto1);
 
 
-const punto = {
-     x: 4,
-     y: 10,
-     dibujar(){
-          console.log("dibujando un ....");
-     }
 
-};
-if('dibujar' in punto){
-     punto.dibujar();
-}
+
+// const punto = {
+//      x: 4,
+//      y: 10,
+//      dibujar(){
+//           console.log("dibujando un ....");
+//      }
+
+// };
+// if('dibujar' in punto){
+//      punto.dibujar();
+//}
 
 //ver la propiedades de un objecto
 //vieja forma
-for(let llave of Object.keys(punto)){
-     console.log(llave, punto[llave]);
-}
+// for(let llave of Object.keys(punto)){
+//      console.log(llave, punto[llave]);
+// }
 
-for(let entry of Object.entries(punto)){
-     console.log(entry);
-}
+// for(let entry of Object.entries(punto)){
+//      console.log(entry);
+// }
 
 
-//nueva forma
-for(let llave in punto){
-     console.log(llave, punto[llave]);
-}
+// //nueva forma
+// for(let llave in punto){
+//      console.log(llave, punto[llave]);
+// }
+
+let nun1 = "15";
+let nun2 = 5;
+
+let total = nun1+nun2;
+
+console.log(total)
